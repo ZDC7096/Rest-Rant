db2b7cf0b46cd2b5d99a88de043ac8ec74dd6fa2
ZDC7096 <108491910+ZDC7096@users.noreply.github.com>
2022-11-15 10:43:17 -0700

    added cs

--- controllers/places.js
+++ controllers/places.js
@@ -25,6 +25,18 @@ router.post('/', (req, res) => {
   places.push(req.body)
   res.redirect('/places')
 })
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id]})
+  }
+})
 
 
 
